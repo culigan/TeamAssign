aa259e946c5b6341e63b882b8a45f3f40f93b676
culigan <culigan@gmail.com>
2019-03-09 13:58:59 -0700

    working I think

--- Week10/index.js
+++ Week10/index.js
@@ -1,106 +1,79 @@
-const express = require('express')
-const path = require('path')
-const lineReader = require('line-reader')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+const { Pool, Client } = require('pg') 
+const connectionString = process.env.DATABASE_URL
+const PORT = process.env.PORT || 5000;
 
+const pool = new Pool({ connectionString: connectionString });
 var app = express();
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.get('/', function (req, res) {
-    res.render('home');
+
+app.get('/', function (request, response) {
+    response.render('home');
 });
-app.get('/cost', determineCost);
+app.get('/getPerson', function (request, response) {
+    getPerson(request, response);
+});
+
         
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-function determineCost(req, res) {
-    var type = req.query.letter;
-    var weight = req.query.weigh;
+function getPerson(request, response) {
+    // First get the person's id
+    var id = request.query.id;
 
-    calculateRate(res, type, weight, this);
-}
+    // TODO: We should really check here for a valid id before continuing on...
 
-function calculateRate(res, type, weight) {
+    // use a helper function to query the DB, and provide a callback for when it's done
+    getPersonFromDb(id, function (error, result) {
+        // This is the callback function that will be called when the DB is done.
+        // The job here is just to send it back.
 
-    let result = 0.0;
-
-    switch (type) {
-        case "stamped":
-            if (weight <= 1.0)
-                result = .55;
-            else if (weight <= 2.0)
-                result = .7;
-            else if (weight <= 3.0)
-                result = .85;
-            else 
-                result = 1.00;
-            break;
-        case "metered":
-            if (weight <= 1.0)
-                result = .5;
-            else if (weight <= 2.0)
-                result = .65;
-            else if (weight <= 3.0)
-                result = .8;
-            else
-                result = .95;
-            break;
-        case "flats":
-            if (weight <= 1.0)
-                result = 1.0;
-            else if (weight <= 2.0)
-                result = 1.15;
-            else if (weight <= 3.0)
-                result = 1.30;
-            else if (weight <= 4.0)
-                result = 1.45;
-            else if (weight <= 5.0)
-                result = 1.60;
-            else if (weight <= 6.0)
-                result = 1.75;
-            else if (weight <= 7.0)
-                result = 1.90;
-            else if (weight <= 8.0)
-                result = 2.05;
-            else if (weight <= 9.0)
-                result = 2.20;
-            else if (weight <= 10.0)
-                result = 2.35;
-            else if (weight <= 11.0)
-                result = 2.5;
-            else if (weight <= 12.0)
-                result = 2.65;
-            else
-                result = 2.80;
-            break;
-        case "retail":
-            //result = calculateZone(type, weight);
-            if (weight <= 1.0)
-                result = 3.66;
-            else if (weight <= 4.0)
-                result = 3.66;
-            else if (weight <= 8.0)
-                result = 4.39;
-            else if (weight <= 6.0)
-                result = .85;
-            else if (weight <= 12.0)
-                result = 5.19;
-            else
-                result = 5.71;
-            break;
-
-    }
-    res.render('pages/viewcost', { result: result } );
-}
-
-function calculateZone(type, weigh) {
-    var zones = document.getElementById('zone').value;
-
-    lineReader.eachLine('/retail.txt', function (line, last) {
-        if (line.startsWith(weigh)) {
-            var price = line.split('$');
-            return price[parseInt(zones) - 2];
+        // Make sure we got a row with the person, then prepare JSON to send back
+        if (error || result == null || result.length != 1) {
+            response.status(500).json({ success: false, data: error });
+        } else {
+            var person = result[0];
+            response.status(200).json(result[0]);
         }
     });
 }
+
+// This function gets a person from the DB.
+// By separating this out from the handler above, we can keep our model
+// logic (this function) separate from our controller logic (the getPerson function)
+function getPersonFromDb(id, callback) {
+    console.log("Getting person from DB with id: " + id);
+
+    // Set up the SQL that we will use for our query. Note that we can make
+    // use of parameter placeholders just like with PHP's PDO.
+    var sql = "SELECT id, first, last, birthdate FROM person WHERE id = $1::int";
+
+    // We now set up an array of all the parameters we will pass to fill the
+    // placeholder spots we left in the query.
+    var params = [id];
+
+    // This runs the query, and then calls the provided anonymous callback function
+    // with the results.
+    pool.query(sql, params, function (err, result) {
+        // If an error occurred...
+        if (err) {
+            console.log("Error in query: ")
+            console.log(err);
+            callback(err, null);
+        }
+
+        // Log this to the console for debugging purposes.
+        console.log("Found result: " + JSON.stringify(result.rows));
+
+
+        // When someone else called this function, they supplied the function
+        // they wanted called when we were all done. Call that function now
+        // and pass it the results.
+
+        // (The first parameter is the error variable, so we will pass null.)
+        callback(null, result.rows);
+    });
+
+} // end of getPersonFromDb
