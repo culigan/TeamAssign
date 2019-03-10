3bcda59447882d32d22a9edbdd317a76172b5978
culigan <culigan@gmail.com>
2019-03-09 18:09:44 -0700

    testing

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 const pool = new Pool({ connectionString: connectionString });
 var app = express();
+
 app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
